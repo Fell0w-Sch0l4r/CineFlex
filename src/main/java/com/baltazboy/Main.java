f1edfb6764f1d11a2b154f65814a6cb530ae9745
Ângel Cagiza <angelcagiza@gmail.com>
2023-06-01 23:08:45 +0100

    1st commit

---
+++ src/main/java/com/baltazboy/Main.java
@@ -0,0 +1,39 @@
+package com.baltazboy;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        var apiKey = System.getenv("OMDB_API_KEY");
+
+        var endereco = "https://imdb-api.com/en/API/Top250Movies/" + apiKey;
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        //System.out.println(response.body());
+
+        String json = response.body();
+        System.out.println(json);
+    }
+
+
+}
